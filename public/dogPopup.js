df37c640ab524d88bbf2adfa5ec5b2353b1b5195
willyspinner <wilsonjusuf1998@gmail.com>
2018-02-03 13:30:14 -0800

    doggo popups

---
+++ public/dogPopup.js
@@ -0,0 +1,32 @@
+function getdogPopupString(doggoName,picURL,doggoHref,ownerURL){
+return `<div class="container">
+	<div class="row">
+		<div class="col-lg-3 col-sm-6">
+
+            <div class="card hovercard">
+                <div class="cardheader">
+
+                </div>
+                <div class="avatar">
+                    <img alt="" src="${picURL}" width=150 height=150>
+                </div>
+                <div class="info">
+                    <div class="title">
+                        <a target="_blank" href="${doggoHref}">${doggoName}</a>
+                    </div>
+                    <div class="desc">Lovely Doggo</div>
+                    <div class="desc">Cutie Pie</div>
+                    <div class="desc">Tech Nerd</div>
+                </div>
+                <div class="bottom">
+                    <a  href="${ownerURL}">
+                        My Owner!
+                    </a>
+                </div>
+            </div>
+
+        </div>
+
+	</div>
+</div>`;
+}
