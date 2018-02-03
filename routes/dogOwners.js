142e95dab0a8218618cb4b713005468b5238e14e
willyspinner <wilsonjusuf1998@gmail.com>
2018-02-03 01:30:42 -0800

    credentials

--- routes/dogOwners.js
+++ routes/dogOwners.js
@@ -1,6 +1,14 @@
-
+var credentials =require('../credentials/credentials.js');
 var express = require('express');
 var router = express.Router({mergeParams:true});
-var email = require('../scripts/email');
+var email = require('../scripts/email').expressInterest;
+var bodyParser = require('body-parser');
 
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
+
+router.post('/expressInterest/:id',function(req,res){
+          let sender = req.params.id;
+          email(sender,"wilsonjusuf1998@gmail.com","I want your doggo!",credentials.mailgun)
+})
 module.exports = router;
