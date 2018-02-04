d6a5c1849f56a44ecec72bbc9b7c1ead5776907b
varun1729 <varun.narayanan1729@gmail.com>
2018-02-04 02:24:48 -0800

    did image flip

--- app.js
+++ app.js
@@ -57,13 +57,13 @@ let server = app.listen(app.get('port'), function() {
 });
 
 function distance(lat1, lon1, lat2, lon2) {
-  var p = 0.017453292519943295;    // Math.PI / 180
-  var c = Math.cos;
-  var a = 0.5 - c((lat2 - lat1) * p)/2 + 
-          c(lat1 * p) * c(lat2 * p) * 
-          (1 - c((lon2 - lon1) * p))/2;
+    var p = 0.017453292519943295; // Math.PI / 180
+    var c = Math.cos;
+    var a = 0.5 - c((lat2 - lat1) * p) / 2 +
+        c(lat1 * p) * c(lat2 * p) *
+        (1 - c((lon2 - lon1) * p)) / 2;
 
-  return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+    return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
 }
 
 //*************SOCKET code*******************
@@ -71,88 +71,80 @@ io.attach(server);
 let sockets = []
 io.on('connection', function(socket) {
 
-            console.log('connection callback called.');
-            socket.emit('CONNECTED_USERS_INFO', { 'connected': sockets });
-            socket.broadcast.emit('SPECIAL_MESSAGE_SENT', { 'message': `${socket} connected!` })
+    console.log('connection callback called.');
+    socket.emit('CONNECTED_USERS_INFO', { 'connected': sockets });
+    socket.broadcast.emit('SPECIAL_MESSAGE_SENT', { 'message': `${socket} connected!` })
 
-            socket.on('POSITION_RECEIVED', function(latLng) {
-                    console.log('position received.');
-                    //Note: latLng is a json object of :
-                    //{lat: LATITUDE, lng: LONGITUDE};
-                    let coordinates = [Number(latLng.lng), Number(latLng.lat)];
-                    let tolearance = 1;
+    socket.on('POSITION_RECEIVED', function(latLng) {
+        console.log('position received.');
+        //Note: latLng is a json object of :
+        //{lat: LATITUDE, lng: LONGITUDE};
+        let coordinates = [Number(latLng.lng), Number(latLng.lat)];
+        let tolearance = 1;
 
-                    // Dog.find({
-                    //         "geo.lng": { "$gt": coordinates[0] - 1, "$lt": coordinates[0] + 1 },
-                    //         'geo.lat': { "$gt": coordinates[1] - 1, "$lt": coordinates[1] + 1 },
-                    //     }).limit(10).exec(function(error, foundDogs) {
-                    //         if (error) {
-                    //             console.log(error);
-                    //         } else {
-                    //             console.log(foundDogs);
-                    //         }
-                    //     });
+        Dog.find({}, function(error, foundDogs) {
+            if (error) {
+                console.log('DOGS NOT FOUND :( ');
+            } else {
+                foundDogs.forEach(function(foundDog) {
+                    delta = distance(coordinates[1], coordinates[0], foundDog.geo.lat, foundDog.geo.lng);
+                    foundDog.update({
+                        delta: delta,
+                    }, function(error, updatedDog) {
+                        if (error) {
+                            console.log("COULDN'T SAVE DOG");
+                        }
+                    })
+                });
+            }
 
-
-                    Dog.find({}, function (error, foundDogs){
-                      if (error){
-                        console.log('DOGS NOT FOUND :( ');
-                      } else {
-                        foundDogs.forEach(function(foundDog) {
-                          delta = distance(coordinates[1],coordinates[0],foundDog.geo.lat, foundDog.geo.lng);
-                          foundDog.update({
-                            delta: delta,
-                          },function(error, updatedDog){
-                            if (error){
-                              console.log("COULDN'T SAVE DOG");
-                            }
-                          })
-                        });
-                      }
-
-                    });
-                    Dog.find({}).sort([['delta','ascending']]).exec(function(error, sortedDogs){
-                      if (error){
-                        console.log(error);
-                      } else {
-                        sortedDogs.forEach(function(pup){
-                          console.log(pup.name + ' '+ pup.delta);
-                        });
-                      }
-                    });
+        });
+        Dog.find({}).sort([
+            ['delta', 'ascending']
+        ]).exec(function(error, sortedDogs) {
+            if (error) {
+                console.log(error);
+            } else {
+                //Wilson here is your sorted dogs
+                sortedDogs.forEach(function(pup) {
+                    console.log(pup.name + ' ' + pup.delta);
+                });
+            }
+        });
 
 
-                        /*   Dog.find({
-                               geo: {
-                                       $near: coordinates,
-                             }*/
-                        //let distance = 0.001
+        /*   Dog.find({
+               geo: {
+                       $near: coordinates,
+             }*/
+        //let distance = 0.001
 
 
 
-                        //let queryObject={"$and":[latObject,lngObject]}
+        //let queryObject={"$and":[latObject,lngObject]}
 
-                        //console.log(JSON.stringify(queryObject));
-                        //           queryString = {"$and":[{'geo.lat':{"$lt":(Number(latLng.lat)+distance),"$gt":(Number(latLng.lat)-distance)}},
-                        //           {'geo.lng':{"$lt":(Number(latLng.lng)+distance),"$gt":(Number(latLng.lng)-distance)}}]//queryObject
+        //console.log(JSON.stringify(queryObject));
+        //           queryString = {"$and":[{'geo.lat':{"$lt":(Number(latLng.lat)+distance),"$gt":(Number(latLng.lat)-distance)}},
+        //           {'geo.lng':{"$lt":(Number(latLng.lng)+distance),"$gt":(Number(latLng.lng)-distance)}}]//queryObject
 
-                        // };        //For now, we will bypass this to save time
-                        //            Dog.find(/*queryString*/{}, function(error, foundDogs) {
-                        //             if (error) {
-                        //                 console.log(error);
-                        //                 //IT GOES HERE. THERE IS AN ERROR FINDING DOGs.
-                        //                 console.log('ERROR FINDING DOGS APP JS')
-                        //             } else {
-                        //                 console.log('found dogs object server side:')
-                        //                 console.log(foundDogs);
-                        //                 //  var geospatial_query_result =
-                        //                 socket.emit('DOGS_NEAR_USER', foundDogs); // VARUN. This result undefined. ??
-                        //             }
-                        //  });//.and([latObject,lngObject]);
+        // };        //For now, we will bypass this to save time
+        //            Dog.find(/*queryString*/{}, function(error, foundDogs) {
+        //             if (error) {
+        //                 console.log(error);
+        //                 //IT GOES HERE. THERE IS AN ERROR FINDING DOGs.
+        //                 console.log('ERROR FINDING DOGS APP JS')
+        //             } else {
+        //                 console.log('found dogs object server side:')
+        //                 console.log(foundDogs);
+        //                 //  var geospatial_query_result =
+        //                 socket.emit('DOGS_NEAR_USER', foundDogs); // VARUN. This result undefined. ??
+        //             }
+        //  });//.and([latObject,lngObject]);
 
-                    }); socket.on('SEND_MESSAGE', function(message) {
-                    socket.broadcast.emit('MESSAGE_SENT', message);
-                })
+    });
+    socket.on('SEND_MESSAGE', function(message) {
+        socket.broadcast.emit('MESSAGE_SENT', message);
+    })
 
 
-            });
+});
