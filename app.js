d745d0fbfae5727255c07e12d82152c03806de66
varun1729 <varun.narayanan1729@gmail.com>
2018-02-03 22:01:13 -0800

    set up passport

--- app.js
+++ app.js
@@ -1,87 +1,100 @@
-  //Express set up
-  let express = require("express");
-  app = express();
-  serverPort = 8000;
-  io = require('socket.io')();
+//Express set up
+let express = require("express");
+app = express();
+serverPort = 8000;
+io = require('socket.io')();
+router = require('express-router');
 
-  //BodyParser set up
-  bodyParser = require("body-parser");
-  app.use(bodyParser.urlencoded({ extended: true }));
+//Passport JS setup
+let passport = require('passport');
+LocalStrategy = require('passport-local').Strategy;
 
+//BodyParser set up
+bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  //Mongoose set up
-  mongoose = require("mongoose");
-  mongoose.connect("mongodb://localhost/Woof");
+//Mongoose set up
+mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/Woof");
 
-  //Models settings
-  Dog = require("./models/dog");
-  User = require("./models/user");
+//Models settings
+Dog = require("./models/dog");
+User = require("./models/user");
 
-  //Seed file set up and run!
-  seed = require("./models/seed");
-  seed();
+//Seed file set up and run!
+seed = require("./models/seed");
+seed();
 
-  //Routes
-  app.use('/public', express.static(__dirname + '/public'));
-  app.use(require('./routes/dogOwners'));
-  app.use(require('./routes/landing'));
-  app.use(require('./routes/loginAndRegister'));
-  app.use(require('./routes/map'));
-  app.use(require('./routes/dog'));
+//App config
+app.use(require('express-session')({
+    secret: 'I wanna go poopiee',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-  //req.user avalaible to all routes
-  app.use(function(req, res, next) {
-      res.locals.currentUser = req.user;
-      next();
-  });
+//Route config
+app.use('/public', express.static(__dirname + '/public'));
+app.use(require('./routes/dogOwners'));
+app.use(require('./routes/landing'));
+app.use(require('./routes/loginAndRegister'));
+app.use(require('./routes/map'));
+app.use(require('./routes/dog'));
 
-  //App settings
-  app.set('port', serverPort);
-  app.set('view engine', 'ejs');
-  app.set('views', 'views');
-  app.set('sockets', []);
+//App settings
+app.set('port', serverPort);
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.set('sockets', []);
 
-  //Servloger listen:
-  let server = app.listen(app.get('port'), function() {
-      console.log('Listening on port ' + app.get('port'));
-  });
+//Servloger listen:
+let server = app.listen(app.get('port'), function() {
+    console.log('Listening on port ' + app.get('port'));
+});
 
 
   //*************SOCKET code*******************
-  io.attach(server);
-  let sockets = []
-  io.on('connection', function(socket) {
+io.attach(server);
+let sockets = []
+io.on('connection', function(socket) {
 
-      console.log('connection callback called.');
-      socket.emit('CONNECTED_USERS_INFO', { 'connected': sockets });
-      socket.broadcast.emit('SPECIAL_MESSAGE_SENT', { 'message': `${socket} connected!` })
+    console.log('connection callback called.');
+    socket.emit('CONNECTED_USERS_INFO', { 'connected': sockets });
+    socket.broadcast.emit('SPECIAL_MESSAGE_SENT', { 'message': `${socket} connected!` })
 
-      socket.on('POSITION_RECEIVED', function(latLng) {
-          //Note: latLng is a json object of :
-          //{lat: LATITUDE, lng: LONGITUDE};
-          let cordinates = [Number(latlng.lng), Number(latlng.lat)];
-          distance = 100 * 1000; //in meteres
-          point = {
-              type: "Point",
-              cordinates: cordinates,
-          };
-          Dog.find({
-              loc: {
-                  $near: {
-                      $geometry: point,
-                      $maxDistance: distance,
-                  }
-              }
-          }, function(error, foundDogs) {
-            console.log(foundDogs);
-              var geospatial_query_result = foundDogs;
-              socket.emit('DOGS_NEAR_USER', geospatial_query_result);
-          });
+    socket.on('POSITION_RECEIVED', function(latLng) {
+        //Note: latLng is a json object of :
+        //{lat: LATITUDE, lng: LONGITUDE};
+        let cordinates = [Number(latLng.lng), Number(latLng.lat)];
+        distance = 100 * 1000; //in meteres
+        point = {
+            type: "Point",
+            cordinates: cordinates,
+        };
+        Dog.find({
+            loc: {
+                $near: {
+                    $geometry: point,
+                    $maxDistance: distance,
+                }
+            }
+        }, function(error, foundDogs) {
+          console.log(foundDogs);
+            if (error){
+              console.log(error);
+            }
+            var geospatial_query_result = foundDogs;
+            socket.emit('DOGS_NEAR_USER', geospatial_query_result);
+        });
 
-      });
-      socket.on('SEND_MESSAGE', function(message) {
-          socket.broadcast.emit('MESSAGE_SENT', message);
-      })
+    });
+    socket.on('SEND_MESSAGE', function(message) {
+        socket.broadcast.emit('MESSAGE_SENT', message);
+    })
 
 
-  });
+});
