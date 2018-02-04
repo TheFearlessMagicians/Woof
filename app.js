18e2ccf05b39ef748fcd277b44f549a9441a03c7
varun1729 <varun.narayanan1729@gmail.com>
2018-02-03 17:54:03 -0800

    for willy

--- app.js
+++ app.js
@@ -1,68 +1,51 @@
-//Express set up
-let express = require("express");
-app = express();
-serverPort = 8000;
-io = require('socket.io')();
-
-//BodyParser set up
-bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-//Mongoose set up
-mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/Woof");
-
-//Models settings
-Dog = require("./models/dog");
-User = require("./models/user");
-
-//Seed file set up and run!
-seed = require("./models/seed");
-seed();
-
-//Routes
-app.use('/public', express.static(__dirname + '/public'));
-app.use(require('./routes/dogOwners'));
-app.use(require('./routes/landing'));
-app.use(require('./routes/loginAndRegister'));
-app.use(require('./routes/map'));
-app.use(require('./routes/dog'));
-
-//req.user avalaible to all routes
-app.use(function(req,res,next){
-  res.locals.currentUser = req.user;
-  next();
-});
-
-//App settings
-app.set('port', serverPort);
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-app.set('sockets', []);
-
-// //AUTHENTICATION
-// let passport = require("passport");
-// LocalStratergy = require("passport-local");
-
-// //PASSPORT CONFIGURATION
-// app.use(require("express-session")({
-//         secret: "I wanna go poopie!",
-//         resave: false,
-//         saveUninitialized: false,
-//     }
-// ));
-// app.use(passport.initialize());
-// app.use(passport.session());
-// passport.use(new LocalStratergy(User.authenticate()));
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
-
-//Servloger listen:
-let server = app.listen(app.get('port'), function() {
-    console.log('Listening on port ' + app.get('port'));
-});
-
+  //Express set up
+  let express = require("express");
+  app = express();
+  serverPort = 8000;
+  io = require('socket.io')();
+  
+  //BodyParser set up
+  bodyParser = require("body-parser");
+  app.use(bodyParser.urlencoded({ extended: true }));
+  
+  
+  //Mongoose set up
+  mongoose = require("mongoose");
+  mongoose.connect("mongodb://localhost/Woof");
+  
+  //Models settings
+  Dog = require("./models/dog");
+  User = require("./models/user");
+  
+  //Seed file set up and run!
+  seed = require("./models/seed");
+  seed();
+  
+  //Routes
+  app.use('/public', express.static(__dirname + '/public'));
+  app.use(require('./routes/dogOwners'));
+  app.use(require('./routes/landing'));
+  app.use(require('./routes/loginAndRegister'));
+  app.use(require('./routes/map'));
+  app.use(require('./routes/dog'));
+  
+  //req.user avalaible to all routes
+  app.use(function(req,res,next){
+    res.locals.currentUser = req.user;
+    next();
+  });
+  
+  //App settings
+  app.set('port', serverPort);
+  app.set('view engine', 'ejs');
+  app.set('views', 'views');
+  app.set('sockets', []);
+  
+  //Servloger listen:
+  let server = app.listen(app.get('port'), function() {
+      console.log('Listening on port ' + app.get('port'));
+  });
+  
 
 //*************SOCKET code*******************
 io.attach(server);
